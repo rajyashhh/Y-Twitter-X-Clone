a515ca8e0c707c5fd26caa2ed526c3ad2650b864
Yash Raj Singh <workmail.yrs@gmail.com>
2025-04-18 23:48:23 +0530

    minor change

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,29 @@ import dotenv from "dotenv"
 import connectMongoDB from "./db/connectMongoDB.js";
 import mongoose from "mongoose";
 import cors from 'cors';
+import cookieParser from "cookie-parser";
 const app = express();
 dotenv.config(); 
 
-const PORT = process.env.PORT || 5000;
-app.use('/api/auth', authRoutes);
+const PORT = 3000;
 
 
+const corsOptions = {
+    // methods : ["GET","POST"],
+    origin: "*",
+    credentials: true
+}
+
+app.use(cors(corsOptions))
 app.use(express.json());
+app.use(cookieParser())
 
 app.get("/", (req,res)=>{
+    console.log("req received")
     res.send("Server is ready");
 })
+
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
