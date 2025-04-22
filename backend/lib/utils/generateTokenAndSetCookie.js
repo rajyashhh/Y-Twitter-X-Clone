cfe7fe18f8d8aa38ea0d538689592005036bfcae
Yash Raj Singh <workmail.yrs@gmail.com>
2025-04-22 18:34:35 +0530

    minor change

--- backend/lib/utils/generateTokenAndSetCookie.js
+++ backend/lib/utils/generateTokenAndSetCookie.js
@@ -8,6 +8,6 @@ export const generateTokenAndSetCookie = async (userId,res)=>{
         maxAge: 10*24*60*60*1000,
         httpOnly: true,
         sameSite: "strict",
-        // secure : process.env.NODE_ENV !== "development"
+        secure : process.env.NODE_ENV !== "development"
     })
 }
