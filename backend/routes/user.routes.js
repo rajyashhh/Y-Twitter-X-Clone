faca3cde3d2da463dbb8375abc276b85d7664cd1
Yash Raj Singh <workmail.yrs@gmail.com>
2025-04-24 01:24:01 +0530

    follow unfollow logic added

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,13 +1,13 @@
 import express from "express";
-import protectedRoute from "../middlewares/protectedRoute";
+import protectedRoute from "../middlewares/protectedRoute.js";
+import { getUserProfile, followUnfollowUser } from "../controllers/user.controller.js";
 
-
-const router = express.router();
+const router = express.Router();
 
 router.get("/profile/:username",protectedRoute, getUserProfile);
 router.get("/suggested",protectedRoute, getUserProfile);
 router.post("/follow/:id", protectedRoute, followUnfollowUser);
-router.post("/update", protectedRoute, updateuserProfile)
+//router.post("/update", protectedRoute, updateuserProfile);
 
 
 
