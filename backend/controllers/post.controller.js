d01534abab6ca236a12eb83ffb4844fd421c26ef
Yash Raj Singh <workmail.yrs@gmail.com>
2025-04-24 23:28:38 +0530

    comment option added

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -67,4 +67,31 @@ const deletePost = async (req,res)=>{
     }
 }
 
-export {createPost, deletePost};
+
+const commentOnPost = async (req,res)=>{
+    try {
+        const {text} = req.body;
+        const postId = req.params.id;
+        const userId = req.user._id;
+
+        if(!text){
+            return res.status(400).json({error: "Post not found"})
+        }
+        const post = await Post.findById(postId);
+        if(!post){
+            return res.status(404).json({
+                error : "POst not found"
+            })
+        }
+        const comment = {user: userId, text};
+        post.comments.push(comment);
+        await post.save();
+        res.status(200).json(post);
+    } catch (error) {
+        console.log("Error in commentOnPost controller: ", error.message);
+        res.status(500).json({
+            error: "Internal Server error"
+        })
+    }
+}
+export {createPost, deletePost, commentOnPost};
