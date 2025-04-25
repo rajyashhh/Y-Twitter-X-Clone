a092971437d97b5f381c47b3789aac163939ebc2
Yash Raj Singh <workmail.yrs@gmail.com>
2025-04-25 15:42:06 +0530

    like unline feature added

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import Post from "../models/post.model.js"
 import cloudinary from "cloudinary";
+import Notification from "../models/notification.model.js";
 const createPost = async (req,res)=>{
     try {
         const {text} = req.body;
@@ -94,4 +95,42 @@ const commentOnPost = async (req,res)=>{
         })
     }
 }
-export {createPost, deletePost, commentOnPost};
+
+
+const likeUnlikePost = async (req,res)=>{
+    try {
+        const userId = req.user._id;
+        const {id:postId} = req.params;
+        const post = await Post.findById(postId);
+        if(!post){
+            return res.status(404).json({
+                error : "Post not found"
+            })
+        }
+
+        const userLikedPost = post.likes.includes(userId);
+        if(userLikedPost){
+            //Unlike the post
+            await Post.updateOne({_id: postId}, {$pull: {likes: userId}})
+            res.status(200).json({message: "Post unliked successfully"})
+        }
+        else{
+            //like the post
+            post.likes.push(userId);
+            await post.save();
+            const notification = new Notification({
+                from: userId,
+                to: post.user,
+                type: "like"
+            })
+            await notification.save();
+            res.status(200).json({message: "Post liked successfully"})
+        }
+        } catch (error) {
+            console.log("Error in likeUnlinePost controller: ", error.message);
+            res.status(500).json({
+                error: "Internal server error!"
+            })
+    }
+}
+export {createPost, deletePost, commentOnPost, likeUnlikePost};
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import protectedRoute from "../middlewares/protectedRoute.js";
-import { createPost, deletePost, commentOnPost } from "../controllers/post.controller.js";
+import { createPost, deletePost, commentOnPost, likeUnlikePost } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create",protectedRoute,createPost);
-// router.post("/like/:id", protectedRoute, likeUnlinePost);
+router.post("/like/:id", protectedRoute, likeUnlikePost);
 router.post("/comment/:id", protectedRoute, commentOnPost);
 router.delete("/:id",protectedRoute,deletePost);
 
