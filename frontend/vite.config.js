b4e4ed54e957f2a900b4e88966fa7fb3f4469885
Yash Raj Singh <workmail.yrs@gmail.com>
2025-04-26 22:54:21 +0530

    dded so me assets and installed daisy ui

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// import dotenv from "dotenv"
+// dotenv.config(); 
+// const PORT = process.env.PORT;
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  
+  plugins: [
+    tailwindcss(),
+    react()],
+  server:{
+    port: 3000
+  }
 })
