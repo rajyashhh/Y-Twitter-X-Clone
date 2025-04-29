a34cb70388937b067497def1cfcdcc56535ad4b5
Yash Raj Singh <workmail.yrs@gmail.com>
2025-04-29 17:00:27 +0530

    ui added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,21 @@ import {Route, Routes} from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import SignupPage from "./pages/auth/signup/SignupPage";
 import LoginPage from "./pages/auth/Login/LoginPage";
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
 
 function App(){
   return(
-    <div className="flex max-w-10xl mx-auto">
+    <div className="flex max-w-5xl mx-auto">
+      <Sidebar/>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/signup' element={<SignupPage/>}/>
         <Route path='/login' element={<LoginPage/>}/>
       </Routes>
+      <RightPanel/>
     </div>
+    
 
   )
 }
