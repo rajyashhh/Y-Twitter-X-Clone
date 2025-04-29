ac31d15ab55f2ac23ef5c70c09da8be590f5c700
Yash Raj Singh <workmail.yrs@gmail.com>
2025-04-29 15:41:57 +0530

    added login page and signup page

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,14 +1,45 @@
-import React from 'react'
+import { useState } from "react";
 
-function HomePage() {
-  return (
-    
-    <div>HomePage
+import Posts from "../../components/common/Posts";
+import CreatePost from "./CreatePost";
 
+const HomePage = () => {
+	const [feedType, setFeedType] = useState("forYou");
 
-        <h1> Hello worlddd!!!</h1>
-    </div>
-  )
-}
+	return (
+		<>
+			<div className='flex-[4_4_0] mr-auto border-r border-gray-700 min-h-screen'>
+				{/* Header */}
+				<div className='flex w-full border-b border-gray-700'>
+					<div
+						className={
+							"flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
+						}
+						onClick={() => setFeedType("forYou")}
+					>
+						For you
+						{feedType === "forYou" && (
+							<div className='absolute bottom-0 w-10  h-1 rounded-full bg-primary'></div>
+						)}
+					</div>
+					<div
+						className='flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative'
+						onClick={() => setFeedType("following")}
+					>
+						Following
+						{feedType === "following" && (
+							<div className='absolute bottom-0 w-10  h-1 rounded-full bg-primary'></div>
+						)}
+					</div>
+				</div>
 
-export default HomePage
+				{/*  CREATE POST INPUT */}
+				<CreatePost />
+
+				{/* POSTS */}
+				<Posts />
+			</div>
+		</>
+	);
+};
+export default HomePage;
