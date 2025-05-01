800e006c480a7255d5e1a0e77f70983ca7ead446
Yash Raj Singh <workmail.yrs@gmail.com>
2025-05-01 23:50:30 +0530

    minor change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,11 @@ import {Route, Routes} from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import SignupPage from "./pages/auth/signup/SignupPage";
 import LoginPage from "./pages/auth/Login/LoginPage";
+
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
 import Sidebar from "./components/common/Sidebar";
 import RightPanel from "./components/common/RightPanel";
-
 function App(){
   return(
     <div className="flex max-w-5xl mx-auto">
@@ -14,6 +16,8 @@ function App(){
         <Route path='/' element={<HomePage/>}/>
         <Route path='/signup' element={<SignupPage/>}/>
         <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/notifications' element={<NotificationPage/>}/>
+        <Route path='/profile/:username' element={<ProfilePage/>}/>
       </Routes>
       <RightPanel/>
     </div>
