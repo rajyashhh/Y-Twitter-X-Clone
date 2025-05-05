6fb0cbe46eab2b87557b8e8cd93a310340ef1daf
Yash Raj Singh <workmail.yrs@gmail.com>
2025-05-05 23:29:19 +0530

    minor add ons

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,6 +12,12 @@ export default defineConfig({
     tailwindcss(),
     react()],
   server:{
-    port: 3000
+    port: 3000,
+    proxy:{
+      "/api":{
+        target:"http:localhost:5000",
+        changeOrigin: true,
+      }
+    }
   }
 })
