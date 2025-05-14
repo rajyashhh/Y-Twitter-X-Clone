8d45bbace8690aaf3a83908a8009e051ee0b4e00
Yash Raj Singh <workmail.yrs@gmail.com>
2025-05-14 13:17:58 +0530

    minor buf fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,7 +14,6 @@ createRoot(document.getElementById('root')).render(
       <QueryClientProvider client={queryClient}>
       <App /> 
       </QueryClientProvider>
-      
     </BrowserRouter>
     
   </StrictMode>,
