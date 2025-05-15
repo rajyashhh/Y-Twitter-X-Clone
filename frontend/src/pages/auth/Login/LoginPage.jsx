63b16b407e709b050d66dc5f386ae4f0f06164fe
Yash Raj Singh <workmail.yrs@gmail.com>
2025-05-15 19:29:59 +0530

    minor design tweek

--- frontend/src/pages/auth/Login/LoginPage.jsx
+++ frontend/src/pages/auth/Login/LoginPage.jsx
@@ -63,7 +63,7 @@ const LoginPage = () => {
 
 	return (
 		<div className='max-w-screen-xl mx-auto flex h-screen'>
-			<div className='flex-1 hidden lg:flex items-center  justify-center'>
+			<div className='flex-1 hidden lg:flex items-center  justify-center items-center'>
 				<XSvg className='lg:w-2/3 fill-white' />
 			</div>
 			<div className='flex-1 flex flex-col justify-center items-center'>
