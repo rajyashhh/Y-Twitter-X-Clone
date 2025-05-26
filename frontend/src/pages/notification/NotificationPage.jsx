4f1557a3f4efed20c9a697ba54d3c350b27a916a
Yash Raj Singh <workmail.yrs@gmail.com>
2025-05-27 01:45:03 +0530

    Notification Page Added
    Comment notification is still left to fixwd. Self notifications are also coming which should be removed.

--- frontend/src/pages/notification/NotificationPage.jsx
+++ frontend/src/pages/notification/NotificationPage.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 import LoadingSpinner from "../../components/common/LoadingSpinner";
-import {useQuery} from "@tanstack/react-query"
+import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query"
 import { IoSettingsOutline } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
+import toast from "react-hot-toast";
 import { IconUserSquareRounded } from "@tabler/icons-react";
 
 const NotificationPage = () => {
@@ -12,37 +13,44 @@ const NotificationPage = () => {
 		queryKey:["notifications"],
 		queryFn: async()=>{
 			try {
-				
+				const res = await fetch("/api/notifications");
+				const data = res.json();
+				if(!res.ok) {
+					throw new Error(data.error || "Something went wrong");
+				}
+				return data;
 			} catch (error) {
-				
+				throw new Error(error.message);
 			}
 		}
 	})
-
-	const notifications = [
-		{
-			_id: "1",
-			from: {
-				_id: "1",
-				username: "johndoe",
-				profileImg: "/avatars/boy2.png",
-			},
-			type: "follow",
+	const queryClient = useQueryClient();
+	const {mutate: deleteNotifications } = useMutation({
+		mutationFn: async()=>{
+			try {
+				const res = await fetch("/api/notifications",{
+					method: "DELETE"
+				})
+				const data = await res.json();
+				if(!res.ok){
+					throw new Error(data.error || "Something went wrong");
+				}
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		
 		},
-		{
-			_id: "2",
-			from: {
-				_id: "2",
-				username: "janedoe",
-				profileImg: "/avatars/girl1.png",
-			},
-			type: "like",
+		onError: (error) => {
+			toast.error(error.message)
 		},
-	];
+		onSuccess: ()=>{
+			toast.success("Notifications deleted successfully");
+			queryClient.invalidateQueries({queryKey:["notifications"]})
+		}
+	})
 
-	const deleteNotifications = () => {
-		alert("All notifications deleted");
-	};
+
 
 	return (
 		<>
