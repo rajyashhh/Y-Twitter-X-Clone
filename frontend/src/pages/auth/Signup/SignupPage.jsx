e8b2e98c6b3fb6493b50ec6922c36a627595b5b9
Yash Raj Singh <workmail.yrs@gmail.com>
2025-06-07 03:41:36 +0530

    minor bug fix

--- frontend/src/pages/auth/Signup/SignupPage.jsx
+++ frontend/src/pages/auth/Signup/SignupPage.jsx
@@ -7,7 +7,7 @@ import { MdOutlineMail } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
 import { MdPassword } from "react-icons/md";
 import { MdDriveFileRenameOutline } from "react-icons/md";
-import {useMutation} from '@tanstack/react-query';
+import {useMutation, useQueryClient} from '@tanstack/react-query';
 
 
 const SignupPage = () => {
@@ -47,7 +47,7 @@ const SignupPage = () => {
 	
 
 
-
+	const queryClient = useQueryClient();
 	const {mutate, isError, isPending, error} = useMutation({
 		mutationFn: async ({email,username,fullName,password}) => { 
 			try {
